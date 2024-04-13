2439948a22aba9f4e1379fb1cae0dd466d4d371b
Stepan Isayev <140114944+isastep@users.noreply.github.com>
2024-04-13 19:47:47 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,58 @@
+    for (let i = 0; i < 10; i++) {
+}
+    return data;
+
+    data.forEach(item => {
+    }
+    });
+main();
+function main() {
+}
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = generateRandomData();
+
+        console.log("Random Number: " + item);
+
+    const data = [];function main() {
+
+    data.forEach(item => {
+    const data = generateRandomData();
+    }
+}
+    const data = [];
+main();
+}
+        console.log("Random Number: " + item);
+
+    });
+function generateRandomData() {
+    return data;
+    for (let i = 0; i < 10; i++) {
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    main()
+
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+
+def generate_random_data():
+def main():
+if __name__ == "__main__":
+    for item in data:
+    return data
+
+import random        }
+        for (int item : data) {
+}
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+
+
+        }
+    }
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+            data[i] = (int) (Math.random() * 100) + 1;
+        int[] data = new int[10];
